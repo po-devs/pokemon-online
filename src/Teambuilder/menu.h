0cc973758b21111c7ba37c42696cb9640660ef93
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2009-10-22 16:54:15 +0000

    Added the base files for the menu
    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@72 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

---
+++ src/Teambuilder/menu.h
@@ -0,0 +1,18 @@
+#ifndef TEAMBUILDER_MENU_H
+#define TEAMBUILDER_MENU_H
+
+#include "teambuilder.h"
+//for Team struct
+#include "../PokemonInfo.h"
+// :)
+#include <QtGui>
+
+class TB_Menu : public QWidget
+{
+	Q_OBJECT
+private:
+    Team myteam;
+public:
+};
+
+#endif
