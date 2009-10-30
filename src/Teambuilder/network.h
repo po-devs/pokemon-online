151bc486ee330a81f62c36e153c9d18aa5d0a7d3
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2009-10-30 19:14:29 +0000

    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@123 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

--- src/Teambuilder/network.h
+++ src/Teambuilder/network.h
@@ -11,7 +11,11 @@ class Network : public QTcpSocket
     Q_OBJECT
 public:
     Network(const QString &host="", quint16 port=0);
-    /* Useful inherited functions: connectToHost */
+    /* Useful inherited functions:
+           connectToHost(QString&, quint16): connects to host & port
+           error: returns an enum (QAbstractSocket::SocketError) describing the error
+           errorString: returns a readable string describing the error
+           isValid: returns whether the socket is valid or not! */
 public slots:
     void onReceipt();
 signals:
