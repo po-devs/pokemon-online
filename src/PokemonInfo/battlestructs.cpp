77af71e601a0d5243064343885fe322270cbda6e
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2009-11-14 21:36:08 +0000

    Worked on battle structs
    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@153 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

--- src/PokemonInfo/battlestructs.cpp
+++ src/PokemonInfo/battlestructs.cpp
@@ -1,5 +1,6 @@
 #include "battlestructs.h"
 #include "pokemoninfo.h"
+#include "networkstructs.h"
 
 BattleMove::BattleMove()
 {
@@ -8,6 +9,26 @@ BattleMove::BattleMove()
     setPower(0);
     setPP(0);
     setTotalPP(0);
+}
+
+quint16 BattleMove::num() const {
+    return m_num;
+}
+
+quint8 BattleMove::power() const {
+    return m_power;
+}
+
+quint8 BattleMove::PP() const {
+    return m_PP;
+}
+
+quint8 BattleMove::type() const {
+    return m_type;
+}
+
+quint8 BattleMove::totalPP() const {
+    return m_totalPP;
 }
 
 void BattleMove::setNum(quint16 num) {
@@ -30,6 +51,36 @@ void BattleMove::setTotalPP(quint8 totalPP) {
     m_totalPP = totalPP;
 }
 
+void BattleMove::load() {
+    setPower(MoveInfo::Power(num()));
+    setPP(MoveInfo::PP(num()));
+    setTotalPP(PP());
+    setType(MoveInfo::Type(num()));
+}
+
+QDataStream & operator >> (QDataStream &in, BattleMove &mo)
+{
+    quint16 num;
+    quint8 totalPP,PP,power,type;
+
+    in >> num >> PP >> totalPP >> power >> type;
+
+    mo.setNum(num);
+    mo.setTotalPP(totalPP);
+    mo.setPP(PP);
+    mo.setPower(power);
+    mo.setType(type);
+
+    return in;
+}
+
+QDataStream & operator << (QDataStream &out, const BattleMove &mo)
+{
+    out << mo.num() << mo.PP() << mo.totalPP() << mo.power() << mo.type();
+
+    return out;
+}
+
 PokeBattle::PokeBattle()
 {
     setNum(0);
@@ -42,6 +93,31 @@ PokeBattle::PokeBattle()
 
 void PokeBattle::setConfused(bool confused) {
     m_confused = confused;
+}
+
+const BattleMove & PokeBattle::move(int i) const
+{
+    return m_moves[i];
+}
+
+BattleMove & PokeBattle::move(int i)
+{
+    return m_moves[i];
+}
+
+quint16 PokeBattle::num() const
+{
+    return m_num;
+}
+
+quint16 PokeBattle::normalStat(int stat) const
+{
+    return normal_stats[stat-1];
+}
+
+void PokeBattle::setNormalStat(int stat, quint16 i)
+{
+    normal_stats[stat-1] = i;
 }
 
 void PokeBattle::setStatMod(int stat, qint8 mod)
@@ -75,6 +151,109 @@ void PokeBattle::resetStatMods()
     }
 }
 
+quint16 PokeBattle::lifePoints() const
+{
+    return m_lifepoints;
+}
+
+quint16 PokeBattle::totalLifePoints() const
+{
+    return m_totalLifepoints;
+}
+
+void PokeBattle::init(const PokePersonal &poke)
+{
+    PokeGeneral p;
+    p.setNum(poke.num());
+    p.load();
+
+    for (int i = 0; i < 4; i++) {
+	move(i).setNum(poke.move(i));
+	move(i).load();
+    }
+    m_nick = poke.nickname();
+
+    setTotalLifePoints(PokemonInfo::FullStat(poke.nature(), Hp, p.baseStats().baseHp(), poke.level(), poke.hpDV(), poke.hpEV()));
+    setLifePoints(totalLifePoints());
+
+    for (int i = 0; i < 5; i++) {
+	normal_stats[i] = PokemonInfo::FullStat(poke.nature(), i+1, p.baseStats().baseStat(i+1), poke.level(), poke.DV(i+1), poke.EV(i+1));
+    }
+}
+
+QDataStream & operator >> (QDataStream &in, PokeBattle &po)
+{
+    quint16 num,lifePoints;
+
+    in >> num >> lifePoints;
+
+    po.setNum(num);
+    po.setTotalLifePoints(lifePoints);
+    po.setLifePoints(lifePoints);
+
+    for (int i = 0; i < 5; i++) {
+	quint16 st;
+	in >> st;
+	po.setNormalStat(i+1, st);
+    }
+
+    for (int i = 0; i < 4; i++) {
+	in >> po.move(i);
+    }
+
+    return in;
+}
+
+QDataStream & operator << (QDataStream &out, const PokeBattle &po)
+{
+    out << po.num() << po.totalLifePoints();
+
+    for (int i = 0; i < 5; i++) {
+	out << po.normalStat(i+1);
+    }
+
+    for (int i = 0; i < 4; i++) {
+	out << po.move(i);
+    }
+
+    return out;
+}
+
 TeamBattle::TeamBattle()
 {
+}
+
+TeamBattle::TeamBattle(const TeamInfo &other)
+{
+    for (int i = 0; i < 6; i++) {
+	poke(i).init(other.pokemon(i));
+    }
+}
+
+PokeBattle & TeamBattle::poke(int i)
+{
+    return m_pokemons[i];
+}
+
+const PokeBattle & TeamBattle::poke(int i) const
+{
+    return m_pokemons[i];
+}
+
+QDataStream & operator >> (QDataStream &in, TeamBattle &te)
+{
+    for (int i = 0; i < 4; i++) {
+	in >> te.poke(i);
+    }
+
+    return in;
+}
+
+QDataStream & operator << (QDataStream &out, const TeamBattle &te)
+{
+    for (int i = 0; i < 4; i++) {
+	out << te.poke(i);
+    }
+
+    return out;
 }
