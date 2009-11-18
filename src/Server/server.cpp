9fdda374b421b29722e862320f0d9f5a7b03a450
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2009-11-18 00:28:48 +0000

    something weird happened, correcting
    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@161 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -50,7 +50,6 @@ void Server::loggedIn(int id, const QString &name)
 
 void Server::sendBattleCommand(int id, const QByteArray &comm)
 {
-    printLine("command sent, god damn!");
     player(id)->relay().sendBattleCommand(comm);
 }
 
@@ -120,8 +119,8 @@ void Server::startBattle(int id1, int id2)
     player(id1)->startBattle(id2, battle->pubteam(id1));
     player(id2)->startBattle(id1, battle->pubteam(id2));
 
-    battle->start();
     connect(battle, SIGNAL(battleInfo(int,QByteArray)), SLOT(sendBattleCommand(int, QByteArray)));
+    battle->start();
 }
 
 void Server::battleResult(int desc, int winner, int loser)
