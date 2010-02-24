923fbb441df523517373bd73735656d38d42b260
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2010-02-24 22:11:58 +0000

    Fixed yawn 's message display
    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@321 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

--- src/Server/moves.cpp
+++ src/Server/moves.cpp
@@ -3595,7 +3595,7 @@ struct MMYawn : public MM {
 
     static void uas(int s, int t, BS &b) {
         if (poke(b,t).value("YawnCount").toInt() == 0) {
-            b.sendMoveMessage(147,1,s,Pokemon::Normal,t);
+            b.sendMoveMessage(144,1,s,Pokemon::Normal,t);
             poke(b,t)["YawnCount"] = 2;
             addFunction(poke(b,t), "EndTurn", "Yawn", &et);
         }
@@ -3605,7 +3605,7 @@ struct MMYawn : public MM {
         inc(poke(b,s)["YawnCount"], -1);
         int count = poke(b,s)["YawnCount"].toInt();
         if (count > 0) {
-            b.sendMoveMessage(147,0,s);
+            b.sendMoveMessage(144,0,s);
         } else {
             b.inflictStatus(s, Pokemon::Asleep, s);
             removeFunction(poke(b,s),"EndTurn", "Yawn");
