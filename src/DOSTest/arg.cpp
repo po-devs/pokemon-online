c1a7c57ef7492acc7d7b17b4e5b0cdbe82c63284
coyotte508 <coyotte508@a74332b3-73ba-4bb2-a0e2-b824b54a0f6f>
2010-03-31 11:57:22 +0000

    updated the dos bot and added option "turn off antidos"
    git-svn-id: https://pogeymon-online.svn.sourceforge.net/svnroot/pogeymon-online@386 a74332b3-73ba-4bb2-a0e2-b824b54a0f6f

---
+++ src/DOSTest/arg.cpp
@@ -0,0 +1,64 @@
+#include "arg.h"
+
+DosManager::DosManager()
+{
+    t.start(50,this);
+}
+
+void DosManager::timerEvent(QTimerEvent *)
+{
+    IOManager *m = new IOManager();
+    connect(m, SIGNAL(disconnected()), SLOT(removeStuff()));
+    bots.insert(m);
+    qDebug() << "added , " << bots.size();
+}
+
+void DosManager::removeStuff()
+{
+    qDebug() << "removing, " << bots.size();
+    ((IOManager*)sender())->deleteLater();
+    bots.remove((IOManager*)sender());
+    qDebug() << "removed, " << bots.size();
+}
+
+IOManager::IOManager()
+{
+    qDebug() << "Connecting to localhost";
+
+    connect(&a, SIGNAL(connected()), SLOT(connectionEstablished()));
+    connect(&a, SIGNAL(disconnected()), SLOT(goodToDelete()));
+    on = true;
+    a.connectTo("127.0.0.1", 5080);
+}
+
+void IOManager::goodToDelete()
+{
+    qDebug() << "Disconnected";
+    emit disconnected();
+    on = false;
+}
+
+void IOManager::connectionEstablished()
+{
+    qDebug() << "Connection established";
+
+    TrainerTeam t;
+
+    QString nick;
+    for (int i = 0; i < 1; i++) {
+        nick.append( char(( (rand()+clock()) %2) ? 'A' + ((rand()+clock()) % 26) : 'a' + ( (rand()+clock()) % 26)));
+    }
+
+    t.setTrainerNick(nick);
+
+    FullInfo f = {t, true,true,Qt::black};
+
+    a.login(f);
+
+    this->t.start((rand()%10 + 1)*200,this);
+}
+
+void IOManager::timerEvent(QTimerEvent *)
+{
+    emit disconnected();
+}
