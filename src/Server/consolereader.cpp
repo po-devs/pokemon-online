e246a0e95f9b6932ef1ecbb013f6d2ac5181d000
Toni Fadjukoff <lamperi@lamperi.name>
2010-07-12 22:37:22 +0300

    Added support for command line reading in headless server

---
+++ src/Server/consolereader.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "server.h"
+#include "consolereader.h"
+
+ConsoleReader::ConsoleReader(Server* server) : m_Server(server), m_TextStream(stdin)
+{
+}
+
+void ConsoleReader::read(int)
+{ 
+	QString line = m_TextStream.readLine();
+	if(line != "")
+	{
+		m_Server->sendServerMessage(line);
+	}
+}
