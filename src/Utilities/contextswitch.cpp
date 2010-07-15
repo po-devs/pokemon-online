0e5d071f89f464f9d0200fb7509a8a6e13bd91c6
unknown <eliott@.(none)>
2010-07-15 04:27:04 +0200

    Interface partly done and code started

--- src/Utilities/contextswitch.cpp
+++ src/Utilities/contextswitch.cpp
@@ -1,1 +1,50 @@
 #include "contextswitch.h"
+
+QMutex ContextSwitcher::guardian;
+
+ContextSwitcher::ContextSwitcher() : current_context(NULL)
+{
+    create_context(&main_context);
+}
+
+ContextSwitcher::~ContextSwitcher()
+{
+    foreach (ContextCallee *x, contexts) {
+        delete x;
+    }
+    contexts.clear();
+}
+
+void ContextSwitcher::schedule(ContextCallee *c)
+{
+
+}
+
+void ContextSwitcher::yield()
+{
+    if (!current_context)
+        /* asdf! Crash the fool who called that! But no, just return :( */
+        return;
+
+    ContextCallee *tmp = current_context;
+    current_context = NULL;
+    coro_transfer(&tmp->context, &main_context);
+}
+
+
+void ContextSwitcher::create_context(coro_context *c, coro_func function, void *param, void *stack, long stacksize)
+{
+    guardian.lock();
+    coro_create(c, function, param, stack, stacksize);
+    guardian.unlock();
+}
+
+ContextCallee::ContextCallee(long stacksize)
+{
+    stack = malloc(stacksize);
+}
+
+ContextCallee::~ContextCallee()
+{
+    free(stack);
+}
