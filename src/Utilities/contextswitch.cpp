8461e2b289f651e9772f0fcfcc8918f33244f9e0
unknown <eliott@.(none)>
2010-07-16 23:12:12 +0200

    Debugging the server

--- src/Utilities/contextswitch.cpp
+++ src/Utilities/contextswitch.cpp
@@ -194,10 +194,13 @@ void ContextCallee::terminate()
 
 void ContextCallee::wait()
 {
+    qDebug() << "Starting wait of " << this;
     /* Qt does not provide public functions to wait so it might use 100% CPU */
     while (!finished()) {
         ;
     }
+
+    qDebug() << "Ending wait of " << this;
 }
 
 bool ContextCallee::finished()
