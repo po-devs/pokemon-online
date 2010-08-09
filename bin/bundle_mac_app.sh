3aa3613a1cf441ef656eeebdeafc0f923aaa905b
Toni Fadjukoff <lamperi@lamperi.name>
2010-08-09 14:18:10 +0300

    modified bundle_mac_app script to work with pokemoninfo and utilities dylibs

--- bin/bundle_mac_app.sh
+++ bin/bundle_mac_app.sh
@@ -29,3 +29,24 @@ done
 
 echo macdeployqt Pokemon-Online.app
 macdeployqt Pokemon-Online.app
+
+echo Fixing additional libs
+cp libpokemonlib.1.0.0.dylib Pokemon-Online.app/Contents/Frameworks
+cp libutilities.1.0.0.dylib Pokemon-Online.app/Contents/Frameworks
+# change this if your libzip is located elsewhere
+cp /usr/local/lib/libzip.1.dylib Pokemon-Online.app/Contents/Frameworks
+
+cd Pokemon-Online.app/Contents/Frameworks
+ln -s libpokemonlib.1.0.0.dylib libpokemonlib.1.dylib
+ln -s libutilities.1.0.0.dylib libutilities.1.dylib
+
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore libpokemonlib.1.0.0.dylib
+install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui libpokemonlib.1.0.0.dylib
+install_name_tool -change QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml libpokemonlib.1.0.0.dylib
+install_name_tool -change libutilities.1.dylib @executable_path/../Frameworks/libutilities.1.dylib libpokemonlib.1.dylib
+install_name_tool -change /usr/local/lib/libzip.1.dylib @executable_path/../Frameworks/libzip.1.dylib libpokemonlib.1.0.0.dylib
+
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore libutilities.1.0.0.dylib
+install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui libutilities.1.0.0.dylib
+
+echo Done
