9c6200635123dca33f58bb076bb7504fa736b764
coyotte508 <coyotte508@hotmail.com>
2010-09-24 04:50:33 +0200

    Fixed minor things

--- src/Server/moves.cpp
+++ src/Server/moves.cpp
@@ -3721,7 +3721,7 @@ struct MMSmellingSalt : public MM
 
     static void bcd(int s, int t, BS &b) {
         int st = turn(b,s)["SmellingSalt_Arg"].toInt();
-        if (st == 0 || b.poke(t).status() == st) {
+        if ( (st == 0 && b.poke(t).status() != Pokemon::Fine) || b.poke(t).status() == st) {
             tmove(b, s).power = tmove(b, s).power * 2;
 	}
     }
