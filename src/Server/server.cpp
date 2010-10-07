16b04c69dc9f8aebf538c7bfbb75339e5bfb9dd0
coyotte508 <coyotte508@hotmail.com>
2010-10-07 23:49:30 +0200

    Allowed rated battles through challenge for servers that want it

--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -332,6 +332,7 @@ void Server::loadRatedBattlesSettings()
     QSettings s("config", QSettings::IniFormat);
     allowRatedWithSameIp = !s.value("battles_with_same_ip_unrated").toBool();
     diffIpsForRatedBattles = s.value("rated_battles_memory_number").toInt();
+    allowThroughChallenge = s.value("rated_battle_through_challenge").toInt();
 }
 
 void Server::connectToRegistry()
