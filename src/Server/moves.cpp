43d8f9cdeecdcd694187a9da90800c864a62caaa
coyotte508 <coyotte508@hotmail.com>
2010-10-18 14:01:50 +0200

    Fixed Synchro noise

--- src/Server/moves.cpp
+++ src/Server/moves.cpp
@@ -5473,6 +5473,7 @@ struct MMSynchroNoise : public MM
 {
     MMSynchroNoise() {
         functions["BeforeTargetList"] = &btl;
+        functions["DetermineAttackFailure"] = &daf;
     }
 
     static void btl(int s, int, BS &b) {
@@ -5480,6 +5481,8 @@ struct MMSynchroNoise : public MM
 
         for(unsigned x = 0; x < b.targetList.size(); x++) {
             int target = b.targetList[x];
+            if (target == s)
+                continue;
             if (b.hasType(target, b.getType(s, 1)) || b.hasType(target, b.getType(s, 2))) {
                 newList.push_back(target);
             }
