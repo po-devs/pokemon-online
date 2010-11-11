259b9b7ce2550025a70c44044fe71eae68c45534
Tenno Seremel <haruka@serenareem.net>
2010-11-11 07:17:50 +0300

    Server data for session storage.

--- src/Server/sessiondatafactory.h
+++ src/Server/sessiondatafactory.h
@@ -3,15 +3,14 @@
 
 #include <QObject>
 #include <QtScript>
+#include <QString>
 
 class SessionDataFactory : public QObject
 {
 Q_OBJECT
 public:
     explicit SessionDataFactory(QScriptEngine *engineLink);
-    bool isUserFactoryEnabled() { return userFactoryEnabled; }
-    bool isChannelFactoryEnabled() { return channelFactoryEnabled; }
-    void disableAll() { userFactoryEnabled = false; channelFactoryEnabled = false; }
+    void disableAll();
     void handleUserLogIn(int user_id);
     void handleUserLogOut(int user_id);
     void handleChannelCreate(int channel_id);
@@ -20,8 +19,11 @@ public:
 
     Q_INVOKABLE void registerUserFactory(QScriptValue factoryFunction);
     Q_INVOKABLE void registerChannelFactory(QScriptValue factoryFunction);
+    Q_INVOKABLE void registerGlobalFactory(QScriptValue factoryFunction);
     Q_INVOKABLE QScriptValue users(int id);
     Q_INVOKABLE QScriptValue channels(int id);
+    Q_INVOKABLE QScriptValue global();
+    Q_INVOKABLE void identifyScriptAs(QString scriptId);
 
 private:
     QScriptEngine *engine;
@@ -32,6 +34,11 @@ private:
     bool userFactoryEnabled;
     bool channelFactoryEnabled;
     bool initialStateHandled;
+    QString currentScriptId;
+    void clearAll();
+    QScriptValue globalFactoryFunction;
+    QScriptValue globalFactoryStorage;
+    bool globalFactoryEnabled;
 };
 
 #endif // SESSIONDATAFACTORY_H
