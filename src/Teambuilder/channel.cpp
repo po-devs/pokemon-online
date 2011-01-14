e5814d054790e07a69f04db999c1b8a109459b9d
Elyvilon <elyvilon@users.sourceforge.net>
2011-01-14 15:40:31 -0800

    Fixed syntax error

--- src/Teambuilder/channel.cpp
+++ src/Teambuilder/channel.cpp
@@ -508,8 +508,7 @@ void Channel::printHtml(const QString &str)
 {
     QRegExp id(QString("<\\s*([0-9]+)\\s*>"));
     if (str.contains(id) && client->isIgnored(id.cap(1).toInt())){
-            return;
-        }
+        return;
     }
     if (!QApplication::activeWindow()) {
         if (str.contains(QRegExp(QString("<ping */ *>"),Qt::CaseInsensitive))) {
