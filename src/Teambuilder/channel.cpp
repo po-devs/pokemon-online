37150f4e241a5ff87882cf94a1299e3894af0243
Toni Fadjukoff <lamperi@lamperi.name>
2011-03-01 14:12:50 +0200

    Fixed a bug with enabled events.
    Operator precedence made it fail

--- src/Teambuilder/channel.cpp
+++ src/Teambuilder/channel.cpp
@@ -583,7 +583,7 @@ void Channel::removeEvent(int event)
 bool Channel::eventEnabled(int event)
 {
     int& showEvents = events == -1 ? client->showPEvents : events;
-    return showEvents & event > 0;
+    return (showEvents & event) > 0;
 }
 
 void Channel::resetEvents()
