c00debb843b47bf3c1196ecd06bb0fbc24fba669
coyotte508 <coyotte508@hotmail.com>
2011-03-15 15:35:13 +0100

    Fixed compile error

--- src/Server/battle.cpp
+++ src/Server/battle.cpp
@@ -2467,7 +2467,7 @@ void BattleSituation::useAttack(int player, int move, bool specialOccurence, boo
                     if (!sub) {
                         callaeffects(target, player, "UponBeingHit");
                     }
-                    callaeffects(player, source, "UponOffensiveDamageReceived");
+                    callaeffects(target, player, "UponOffensiveDamageReceived");
                     callieffects(target, player, "UponBeingHit");
                 }
 
