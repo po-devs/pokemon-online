de95746a5b16295ee2146839954e87e1796ce146
coyotte508 <coyotte508@hotmail.com>
2011-06-28 18:57:57 +0200

    Fixed Sleep Talk + Counter bug

--- src/Server/moves.cpp
+++ src/Server/moves.cpp
@@ -2001,7 +2001,6 @@ struct MMCounter : public MM
     }
 
     static void uodr(int s, int source, BS &b) {
-
         if (b.gen() >= 4 && tmove(b,source).category != turn(b,s)["Counter_Arg"].toInt()) {
             return;
         }
@@ -2010,7 +2009,7 @@ struct MMCounter : public MM
             return;
         }
 
-        if (turn(b, s)["DamageTakenByAttack"].toInt() <= 0) {
+        if (turn(b, s).value("DamageTakenByAttack").toInt() <= 0) {
             return;
         }
 
@@ -2019,13 +2018,24 @@ struct MMCounter : public MM
     }
 
     static void ms (int s, int, BS &b) {
+        //In GSC Sleep Talk + Counter works
+        if (!turn(b,s).contains("CounterDamage") && b.gen() == 2) {
+            int t = b.slot(b.opponent(b.player(s)));
+
+            if (b.hasMoved(t) && TypeInfo::Category(MoveInfo::Type(move(b, t), 2)) == turn(b,s)["Counter_Arg"].toInt()
+                    && turn(b, s).value("DamageTakenByAttack").toInt() > 0) {
+                turn(b,s)["CounterDamage"] = 2 * turn(b,s)["DamageTakenByAttack"].toInt();
+                turn(b,s)["CounterTarget"] = t;
+            }
+        }
         turn(b,s)["Target"] = turn(b,s)["CounterTarget"];
         tmove(b,s).targets = Move::ChosenTarget;
     }
 
     static void daf (int s, int, BS &b) {
-        if (!turn(b,s).contains("CounterDamage"))
+        if (!turn(b,s).contains("CounterDamage")) {
             turn(b,s)["Failed"] = true;
+        }
     }
 
     static void cad(int s, int, BS &b) {
