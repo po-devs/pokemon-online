d5617520c0aa36e8f63c4a778f78753aed99cf16
Latios <kazuma20@live.com>
2011-07-16 19:04:35 +0200

    Now icon tray only shows if window is minimized, if it's not, it's hidden

--- src/Server/mainwindow.cpp
+++ src/Server/mainwindow.cpp
@@ -16,8 +16,6 @@ MainWindow::MainWindow(Server *myserver, QWidget *parent)
 
     createTrayIcon();
 
-    trayIcon->show();
-
     connect(myserverwidget, SIGNAL(menuBarChanged()), this, SLOT(reloadMenuBar()));
     connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(systemTrayActivated(QSystemTrayIcon::ActivationReason)));
 
@@ -27,14 +25,11 @@ bool MainWindow::event(QEvent *event)
 {
     if(event->type() == QEvent::WindowStateChange) {
         if(isMinimized()) {
-            if(trayIcon->isVisible()) {
-                trayIcon->showMessage("Pokemon Online is Still runnning", "The program will keep running in system Tray. You can left click the icon to get a menu of options.");
-                setParent(NULL, Qt::SubWindow); // We're avoiding to make the Application show again in the Taskbar.
-                hide();
-                event->ignore();
-            }else{
-                event->accept();
-            }
+            trayIcon->show();
+            trayIcon->showMessage("Pokemon Online is Still runnning", "The program will keep running in system Tray. You can left click the icon to get a menu of options.");
+            setParent(NULL, Qt::SubWindow); // We're avoiding to make the Application show again in the Taskbar.
+            hide();
+            event->ignore();
         }
     }
     QMainWindow::event(event);
@@ -46,6 +41,7 @@ void MainWindow::systemTrayActivated(QSystemTrayIcon::ActivationReason reason)
     if(reason == QSystemTrayIcon::Trigger) {
         setParent(NULL, Qt::Window);
         showNormal();
+        trayIcon->hide();
     }
 }
 
@@ -53,6 +49,7 @@ void MainWindow::openWindow()
 {
     setParent(NULL, Qt::Window);
     showNormal();
+    trayIcon->hide();
 }
 
 void MainWindow::reloadMenuBar()
