6b05b16228115fc71bb5fb8bc77bf53e1ec8138d
coyotte508 <coyotte508@hotmail.com>
2011-08-28 17:11:21 +0200

    Changed battle log format

--- src/Teambuilder/basebattlewindow.h
+++ src/Teambuilder/basebattlewindow.h
@@ -215,7 +215,7 @@ public:
     //void playCry(int pokenum);
 
     void printLine(const QString &str, bool silent = false);
-    void printHtml(const QString &str, bool silent = false);
+    void printHtml(const QString &str, bool silent = false, bool newline = true);
     QString name(int spot) const;
     virtual QString nick(int spot) const;
     QString rnick(int spot) const;
