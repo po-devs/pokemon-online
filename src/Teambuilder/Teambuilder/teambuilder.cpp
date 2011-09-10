68c3325fed39e0b85bc0a3f88f13c5886fd6b35f
James S <coyotte508@hotmail.com>
2011-09-10 23:15:28 +0200

    Change the menubar in the teambuilder to show the RBY generation
    Client has been ready for RBY for a long while now. This change
    is just officially "turning it on".

--- src/Teambuilder/Teambuilder/teambuilder.cpp
+++ src/Teambuilder/Teambuilder/teambuilder.cpp
@@ -322,7 +322,7 @@ QMenuBar * TeamBuilder::createMenuBar(MainEngine *w)
     QString genStrings[] = {tr("RBY (&1st gen)"), tr("GSC (&2nd gen)"), tr("Advance (&3rd gen)"),
                             tr("HGSS (&4th gen)"),tr("B/W (&5th gen)")};
 
-    for (int i = 1; i < NUMBER_GENS; i++) {
+    for (int i = GEN_MIN; i < NUMBER_GENS; i++) {
         this->gens[i] = gen->addAction(genStrings[i], this, SLOT(genChanged()));
         this->gens[i]->setCheckable(true);
         this->gens[i]->setProperty("gen", i + GEN_MIN);
