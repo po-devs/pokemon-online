8a119ec4dbe80a9b1e8240bd38bc39ec3a60fc1a
coyotte508 <coyotte508@hotmail.com>
2011-09-11 20:57:23 +0200

    Added some proxy classes for QML to access BattleData info

---
+++ src/BattleManager/battledataaccessor.h
@@ -0,0 +1,73 @@
+#ifndef BATTLEDATAACCESSOR_H
+#define BATTLEDATAACCESSOR_H
+
+#include <QObject>
+#include <QVector>
+#include "../PokemonInfo/pokemonstructs.h"
+
+class BattleData;
+class TeamData;
+class ShallowBattlePoke;
+
+class PokeProxy : public QObject
+{
+    Q_OBJECT
+public:
+    PokeProxy(ShallowBattlePoke *pokemon);
+
+    Q_PROPERTY(QString nick READ nickname)
+    Q_PROPERTY(int status READ status)
+    Q_PROPERTY(Pokemon::uniqueId num READ num)
+    Q_PROPERTY(bool shiny READ shiny)
+    Q_PROPERTY(int gender READ gender)
+    Q_PROPERTY(int level READ level)
+
+    QString nickname();
+    int status();
+    Pokemon::uniqueId num();
+    bool shiny();
+    int gender();
+    int level();
+private:
+    ShallowBattlePoke *pokeData;
+    inline ShallowBattlePoke *d() {return pokeData;}
+};
+
+class TeamProxy : public QObject
+{
+    Q_OBJECT
+public:
+    TeamProxy(TeamData *teamData);
+    ~TeamProxy();
+
+    Q_INVOKABLE PokeProxy* poke(int index) {
+        return pokemons[index];
+    }
+
+    Q_PROPERTY(QString name READ name)
+    QString name();
+
+private:
+    TeamData *teamData;
+
+    QVector<PokeProxy *> pokemons;
+};
+
+class BattleDataProxy : public QObject
+{
+    Q_OBJECT
+public:
+    BattleDataProxy(BattleData *battleData);
+    ~BattleDataProxy();
+
+    Q_INVOKABLE TeamProxy *team(int player) {
+        return teams[player];
+    }
+
+private:
+    BattleData *battleData;
+
+    QVector<TeamProxy*> teams;
+};
+
+#endif // BATTLEDATAACCESSOR_H
--- src/BattleManager/battlescene.qml
+++ src/BattleManager/battlescene.qml
@@ -1,6 +1,30 @@
 import QtQuick 1.0
 
 Rectangle {
-    width: 100
-    height: 62
+    id: rectangle1
+    width: 500
+    height: 400
+    gradient: Gradient {
+        GradientStop {
+            position: 0
+            color: "#cfa50d"
+        }
+
+        GradientStop {
+            position: 0.460
+            color: "#9e7373"
+        }
+
+        GradientStop {
+            position: 0.700
+            color: "#7794a6"
+        }
+
+        GradientStop {
+            position: 1
+            color: "#40598d"
+        }
+    }
+
+
 }
