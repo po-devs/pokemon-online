793d7ca309883ae1d9ccfe45b616469a8d369b67
coyotte508 <coyotte508@hotmail.com>
2011-09-11 23:32:07 +0200

    Made the battle context accessible in the qml file
    The battle data is accessible in QML through the global
    data property. As Qt doesn't like templates and BattleScene
    inherits from one, I had to make another class in a separate
    file, BattleSceneProxy, and pass it as the main object
    for the QML context. Any introducing of functionalities
    will be through that proxy.

--- src/BattleManager/battlescene.h
+++ src/BattleManager/battlescene.h
@@ -1,10 +1,13 @@
 #ifndef BATTLESCENE_H
 #define BATTLESCENE_H
 
+#include <QObject>
 #include "battlecommandmanager.h"
 
 class BattleData;
 class QDeclarativeView;
+class BattleDataProxy;
+class BattleSceneProxy;
 
 class BattleScene: public BattleCommandManager<BattleScene>
 {
@@ -13,11 +16,16 @@ public:
     ~BattleScene();
 
     QDeclarativeView *getWidget();
+    BattleDataProxy *getProxy();
 private:
     BattleData *mData;
     BattleData *data();
 
+    BattleDataProxy *proxy;
+    BattleSceneProxy *mOwnProxy;
+
     QDeclarativeView *mWidget;
 };
+
 
 #endif // BATTLESCENE_H
