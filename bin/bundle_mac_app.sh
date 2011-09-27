6a641c46cdbb0d1b86d4c52d9aed425a88c13134
Toni Fadjukoff <lamperi@lamperi.name>
2011-09-28 03:59:23 +0800

    Bundle-mac-app to work with new battlelib

--- bin/bundle_mac_app.sh
+++ bin/bundle_mac_app.sh
@@ -26,7 +26,7 @@ function bundle_mac_app() {
        [ -d $toDir/db ] && echo rm -r $toDir/db && rm -r $toDir/db
        [ -d $toDir/Themes ] && echo rm -r $toDir/db && rm -r $toDir/Themes
 
-       for file in languages.txt *.qm Music db database Themes
+       for file in languages.txt trans Music db database Themes qml
        do
            echo cp -r $file $toDir
            cp -r $file $toDir
@@ -35,11 +35,15 @@ function bundle_mac_app() {
 
    fworks=$app/Contents/Frameworks
    zlib=libz.1.dylib
-   libzip=libzip.1.dylib
+   libzip=libzip.2.dylib
    pokemonlib_long=libpokemonlib.1.0.0.dylib
    pokemonlib=libpokemonlib.1.dylib
    utilities_long=libutilities.1.0.0.dylib
    utilities=libutilities.1.dylib
+   battlelib_long=libbattlelib.1.0.0.dylib
+   battlelib=libbattlelib.1.dylib
+   
+   qt_prefix=/Users/lamperi/QtSDK/Desktop/Qt/474/gcc/lib/
 
    mkdir -p $fworks
 
@@ -74,9 +78,9 @@ function bundle_mac_app() {
    cp $pokemonlib_long $fworks/$pokemonlib_long
    ln -sf $pokemonlib_long $fworks/$pokemonlib
    install_name_tool -id @executable_path/../Frameworks/$pokemonlib $fworks/$pokemonlib
-   install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $fworks/$pokemonlib
-   install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $fworks/$pokemonlib
-   install_name_tool -change QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $fworks/$pokemonlib
+   install_name_tool -change ${qt_prefix}QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $fworks/$pokemonlib
+   install_name_tool -change ${qt_prefix}QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $fworks/$pokemonlib
+   install_name_tool -change ${qt_prefix}QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $fworks/$pokemonlib
    install_name_tool -change $utilities @executable_path/../Frameworks/$utilities $fworks/$pokemonlib
    path=$(otool -L $fworks/$pokemonlib | grep "/$libzip" | awk '{print $1}')
    if [ -n "$path" ]; then
@@ -93,8 +97,38 @@ function bundle_mac_app() {
    cp $utilities_long $fworks/$utilities_long
    ln -sf $utilities_long $fworks/$utilities
    install_name_tool -id @executable_path/../Frameworks/$utilities $fworks/$utilities
-   install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $fworks/$utilities
-   install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $fworks/$utilities
+   install_name_tool -change ${qt_prefix}QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $fworks/$utilities
+   install_name_tool -change ${qt_prefix}QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $fworks/$utilities
+
+   echo battle lib..
+   if [ ! -e $battlelib_long ]; then
+       echo "Error! No libbattlelib found, please compile it first!"
+       exit 1
+   fi
+   cp $battlelib_long $fworks/$battlelib_long
+   ln -sf $battlelib_long $fworks/$battlelib
+   install_name_tool -id @executable_path/../Frameworks/$battlelib $fworks/$battlelib
+   install_name_tool -change ${qt_prefix}QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $fworks/$battlelib
+   install_name_tool -change ${qt_prefix}QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $fworks/$battlelib
+   install_name_tool -change ${qt_prefix}QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL $fworks/$battlelib
+   install_name_tool -change ${qt_prefix}QtDeclarative.framework/Versions/4/QtDeclarative @executable_path/../Frameworks/QtDeclarative.framework/Versions/4/QtDeclarative $fworks/$battlelib
+   install_name_tool -change $utilities @executable_path/../Frameworks/$utilities $fworks/$battlelib
+   install_name_tool -change $pokemonlib @executable_path/../Frameworks/$pokemonlib $fworks/$battlelib
+
+   shadersplugin=Pokemon-Online.app/Contents/imports/Qt/labs/shaders/libqmlshadersplugin.dylib
+   install_name_tool -id @executable_path/../imports/Qt/labs/shaders/libqmlshadersplugin.dylib $shadersplugin
+   install_name_tool -change ${qt_prefix}QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $shadersplugin
+   install_name_tool -change ${qt_prefix}QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $shadersplugin
+   install_name_tool -change ${qt_prefix}QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL $shadersplugin
+   install_name_tool -change ${qt_prefix}QtDeclarative.framework/Versions/4/QtDeclarative @executable_path/../Frameworks/QtDeclarative.framework/Versions/4/QtDeclarative $shadersplugin
+   particleplugin=Pokemon-Online.app/Contents/imports/Qt/labs/particles/libqmlparticlesplugin.dylib
+   install_name_tool -id @executable_path/../imports/Qt/labs/shaders/libqmlparticlesplugin.dylib $particleplugin
+   install_name_tool -change ${qt_prefix}QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $particleplugin
+   install_name_tool -change ${qt_prefix}QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $particleplugin
+   install_name_tool -change ${qt_prefix}QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL $particleplugin
+   install_name_tool -change ${qt_prefix}QtDeclarative.framework/Versions/4/QtDeclarative @executable_path/../Frameworks/QtDeclarative.framework/Versions/4/QtDeclarative $particleplugin
+   install_name_tool -change ${qt_prefix}QtSvg.framework/Versions/4/QtSvg @executable_path/../Frameworks/QtSvg.framework/Versions/4/QtSvg $particleplugin
+   install_name_tool -change ${qt_prefix}QtScript.framework/Versions/4/QtScript @executable_path/../Frameworks/QtScript.framework/Versions/4/QtScript $particleplugin
 
    if [ $app == Server.app -a -e serverplugins ]
    then
@@ -109,6 +143,7 @@ function bundle_mac_app() {
            install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $plugin
        done
    fi
+
 
    echo Done
 }
