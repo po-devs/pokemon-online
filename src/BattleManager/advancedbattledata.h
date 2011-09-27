2d6b8f12e410daef7632c2ff08adea64ccb7e657
coyotte508 <coyotte508@hotmail.com>
2011-09-27 13:04:40 +0200

    Added necessary utilities for battle window to know about stat boosts
    The stat boosts, instead of a simple array, could be implemented
    with a declarative list or whatever structure that allows to
    notify the change of one of its elements.
    
    Maybe in the future if we have a more advanced battle window,
    it'll be that, but for now sticking to simple thing. Don't hesitate
    to make the change.

--- src/BattleManager/advancedbattledata.h
+++ src/BattleManager/advancedbattledata.h
@@ -13,6 +13,17 @@ public:
 
     }
 
+    void onStatBoost(int spot, int stat, int boost, bool silent) {
+        (void) silent;
+        fieldPoke(spot).boostStat(stat, boost);
+    }
+
+    void onDynamicInfo(int spot, BattleDynamicInfo info) {
+        for (int i = 0; i < 7; i++) {
+            fieldPoke(spot).setBoost(i+1, info.boosts[i]);
+        }
+    }
+
     void onStartWeather(int spot, int weather, bool ability) {
         (void) ability;
         (void) spot;
