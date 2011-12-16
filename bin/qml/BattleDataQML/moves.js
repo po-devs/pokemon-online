4e0df701a1846130d9610a9c80340232280c4224
coyotte508 <coyotte508@hotmail.com>
2011-12-16 22:57:12 +0100

    used paramters for leech seed, rocks, spikes, tspikes

--- bin/qml/BattleDataQML/moves.js
+++ bin/qml/BattleDataQML/moves.js
@@ -3,15 +3,36 @@ var components = {};
 var effects = {
 };
 
-effects[BattleData.LeechSeed] = "Moves/LeechSeed.qml";
+effects[BattleData.LeechSeed] = "Moves/HiddenPebbles.qml";
+effects[BattleData.StealthRock] = effects[BattleData.LeechSeed];
+effects[BattleData.ToxicSpikes] = effects[BattleData.LeechSeed];
+effects[BattleData.Spikes] = effects[BattleData.LeechSeed];
+
 effects[BattleData.Earthquake] = "Moves/Earthquake.qml";
-effects[BattleData.StealthRock] = "Moves/HiddenPebbles.qml";
-effects[BattleData.ToxicSpikes] = "Moves/ToxicSpikes.qml";
-effects[BattleData.Spikes] = "Moves/Spikes.qml";
 effects[BattleData.RapidSpin] = "Moves/RapidSpin.qml";
 effects[BattleData.TakeDown] = "Moves/TakeDown.qml";
 effects[BattleData.QuickAttack] = "Moves/QuickAttack.qml";
 effects[BattleData.U_turn] = "Moves/UTurn.qml";
+
+var params = {
+};
+
+params[BattleData.LeechSeed] = {"image":"leech-seed.gif",
+    "curves": [{"pos1":{"x":40, "y":10}, "pos2":{"x":0, "y":40}, "controlY":80},
+               {"pos1":{"x":30, "y":5}, "pos2":{"x":30, "y":20}, "controlY":80},
+               {"pos1":{"x":40, "y":15}, "pos2":{"x":70, "y":60}, "controlY":70}]};
+params[BattleData.StealthRock] = {"image":"stealth-rock.png",
+    "curves": [{"pos1":{"x":40, "y":10}, "pos2":{"x":-10, "y":40}, "controlY":80},
+               {"pos1":{"x":30, "y":5}, "pos2":{"x":30, "y":80}, "controlY":80},
+               {"pos1":{"x":40, "y":15}, "pos2":{"x":70, "y":60}, "controlY":70}]};
+params[BattleData.ToxicSpikes] = {"image":"toxic-spikes.png",
+    "curves": [{"pos1":{"x":40, "y":10}, "pos2":{"x":0, "y":55}, "controlY":80},
+               {"pos1":{"x":30, "y":5}, "pos2":{"x":30, "y":65}, "controlY":80},
+               {"pos1":{"x":40, "y":15}, "pos2":{"x":70, "y":60}, "controlY":70}]};
+params[BattleData.Spikes] = {"image":"spikes.png",
+    "curves": [{"pos1":{"x":40, "y":10}, "pos2":{"x":0, "y":55}, "controlY":80},
+               {"pos1":{"x":30, "y":5}, "pos2":{"x":30, "y":65}, "controlY":80},
+               {"pos1":{"x":40, "y":15}, "pos2":{"x":70, "y":60}, "controlY":70}]};
 
 function useAttack(attacker, attack, defender) {
     launchMove(attacker, attack, defender);
@@ -39,7 +60,14 @@ function launchMove(attacker, attack, defender) {
         c = components[key];
     }
 
-    var obj = c.createObject(attacker, {"attacker":attacker, "attack":attack, "target":defender});
+    var p = {"attacker":attacker, "attack":attack, "target":defender};
+    if (key in params) {
+        p["params"] = params[key];
+    } else {
+        p["params"] = {};
+    }
+
+    var obj = c.createObject(attacker, p);
     battle.scene.pause();
     obj.finished.connect(function() {battle.scene.unpause();obj.destroy();});
     obj.start();
