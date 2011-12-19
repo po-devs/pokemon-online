655952392026b73b3becbdf1c74fd7fec56da62d
Toni Fadjukoff <lamperi@lamperi.name>
2011-12-19 22:23:51 +0200

    Began work on Theme Manager.
    This will be a plugin to download and install themes
    automatically to PO.

---
+++ src/ThemeManager/themewidget.h
@@ -0,0 +1,40 @@
+#ifndef THEMEWIDGET_H
+#define THEMEWIDGET_H
+
+#include <QWidget>
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+namespace Ui {
+    class ThemeWidget;
+}
+
+class ThemeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ThemeWidget(QString name, QString author, QString version, QString downloadUrl, QString forumId);
+
+    ~ThemeWidget();
+
+    void setThemeImage(QString image);
+
+private slots:
+    void forumThreadButtonClicked();
+
+    void downloadButtonClicked();
+
+    void downloadFinished(QNetworkReply*);
+
+private:
+    QNetworkAccessManager manager;
+
+    QString downloadUrl;
+    QString forumId;
+
+    Ui::ThemeWidget *ui;
+};
+
+#endif // THEMEWIDGET_H
