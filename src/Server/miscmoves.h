e39b5641857078a85777e71d67cd1e685d1de0f1
Benjamin Lee <kirbyfreak1998@gmail.com>
2011-12-27 20:34:41 +1100

    Disable will now disable a random move in Gen 1.
    Source: http://upokecenter.dreamhosters.com/games/rby/guides/explanations.php#pageid4CBB8D1036
    Also re-commited OHKO mechanics in gen 1 which were lost for some reason.

--- src/Server/miscmoves.h
+++ src/Server/miscmoves.h
@@ -27,6 +27,18 @@ struct MMDisable : public MM
         if (b.counters(t).hasCounter(BC::Disable)) {
             return true;
         }
+
+        /* Gen 1 Disable works even when opponent hasn't moved yet, but not with 0 PP in all moves */
+        if (b.gen() == 1) {
+            for (int i = 0; i<4; i++) {
+                if (b.PP(t, i) > 0) {
+                    return false;
+                }
+            }
+            /* All moves have 0PP, therefore move fails */
+            return true;
+        }
+
         if (!poke(b,t).contains("LastMoveUsedTurn")) {
             return true;
         }
@@ -57,6 +69,20 @@ struct MMDisable : public MM
 
     static void uas (int s, int t, BS &b) {
         int mv = poke(b,t)["LastMoveUsed"].toInt();
+        /* Disable disables a random move in gen 1 */
+        if (b.gen() == 1) {
+            int randnum = b.randint(4);
+            mv = 0;
+            /* This code checks if the user has less than 4 moves so it doesn't pick an empty moveslot */
+            while (mv == 0) {
+                mv = b.move(t,randnum);
+                /* Checks that move doesn't have 0 PP */
+                if (b.PP(t, randnum) == 0) {
+                    mv = 0;
+                }
+                randnum = b.randint(4);
+            }
+        }
         b.sendMoveMessage(28,0,s,0,t,mv);
         if (b.gen() >= 5 && b.hasWorkingItem(t, Item::MentalHerb)) /* mental herb*/ {
             b.sendItemMessage(7,t);
