f0965f248870d74b655159d0b9b4c0a6c1b0f9fb
coyotte508 <coyotte508@hotmail.com>
2012-01-03 23:19:06 +0100

    fixed multiple screen bug

--- src/Utilities/functions.cpp
+++ src/Utilities/functions.cpp
@@ -72,8 +72,13 @@ void loadSettings(QWidget *w, const QSize &defaultSize)
     settings.beginGroup(w->metaObject()->className());
     if (settings.contains("size") || !defaultSize.isNull())
         w->topLevelWidget()->resize(settings.value("size", defaultSize).toSize());
-    if (settings.contains("pos"))
-        w->topLevelWidget()->move(settings.value("pos").toPoint());
+    if (settings.contains("pos")) {
+        QPoint pos = settings.value("pos").toPoint();
+        /* Checks if the position stored is not off the screen */
+        if (QApplication::desktop()->screenGeometry(pos).contains(pos)) {
+            w->topLevelWidget()->move(settings.value("pos").toPoint());
+        }
+    }
     if (settings.contains("maximized") && settings.value("maximized").toBool())
         w->topLevelWidget()->showMaximized();
     settings.endGroup();
