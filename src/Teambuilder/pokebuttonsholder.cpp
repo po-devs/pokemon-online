a388d91f9e495c79ae9967106a67151815bfbbbf
coyotte508 <coyotte508@hotmail.com>
2012-01-06 20:04:28 +0100

    started working on trainer menu

--- src/Teambuilder/pokebuttonsholder.cpp
+++ src/Teambuilder/pokebuttonsholder.cpp
@@ -6,6 +6,17 @@ PokeButtonsHolder::PokeButtonsHolder(QWidget *parent) :
     ui(new Ui::PokeButtonsHolder)
 {
     ui->setupUi(this);
+
+    pokemonButtons[0] = ui->pokemon0;
+    pokemonButtons[1] = ui->pokemon1;
+    pokemonButtons[2] = ui->pokemon2;
+    pokemonButtons[3] = ui->pokemon3;
+    pokemonButtons[4] = ui->pokemon4;
+    pokemonButtons[5] = ui->pokemon5;
+
+    for (int i = 0; i < 6; i++) {
+        pokemonButtons[i]->setNumber(i);
+    }
 }
 
 PokeButtonsHolder::~PokeButtonsHolder()
