5bf833a3620731659e12de586959167480f10295
Benjamin Lee <kirbyfreak1998@gmail.com>
2012-01-09 11:04:08 +1100

    Fixed Issue 56 (Fixed wifi reordering + Challenge Cup)
    Tier check will not be performed if CC is on challenge window.

--- src/Server/battle.cpp
+++ src/Server/battle.cpp
@@ -1601,7 +1601,7 @@ bool BattleSituation::validChoice(const BattleChoice &b)
 
         if (tier().length() > 0) {
             team(player).setIndexes(b.choice.rearrange.pokeIndexes);
-            if (!TierMachine::obj()->isValid(team(player), tier())) {
+            if (!TierMachine::obj()->isValid(team(player), tier()) && !ChallengeInfo::ChallengeCup) {
                 team(player).resetIndexes();
                 return false;
             } else {
