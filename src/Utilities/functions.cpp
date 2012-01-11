fdbf831915b246ec07aa449ce13d26ee2ecb3883
Benjamin Lee <kirbyfreak1998@gmail.com>
2012-01-11 13:35:41 +1100

    Compressed code and improved RTL/Zalgo detector efficency.
    Now it repeats the check at the position a character was found at instead of starting again.

--- src/Utilities/functions.cpp
+++ src/Utilities/functions.cpp
@@ -58,16 +58,11 @@ QString removeTrollCharacters(const QString& s)
 {
     // All Non-Spacing Mark characters are banned and will trigger this filter.
     QString result = s;
-    QChar const rtloverride(0x202e);
     for (int x = 0;x<result.size();x++) {
-        if ((result.at(x)).category() == QChar::Mark_NonSpacing) {
+        if ((result.at(x)).category() == QChar::Mark_NonSpacing || (result.at(x)) == QChar(0x202e)) {
             result.replace(result.at(x), "");
-            x = 0;
+            x--;
         }
-    }
-    // Removes RTL override
-    if (result.contains(rtloverride)) {
-        result.replace(rtloverride, "");
     }
     return result;
 }
