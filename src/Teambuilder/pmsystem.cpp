c96820a7aa99fb05d741ff9a997d5053ea6fa166
Forgive <forgive0@hotmail.com>
2012-01-18 11:39:06 +0800

    Added Tabbed PMs

---
+++ src/Teambuilder/pmsystem.cpp
@@ -0,0 +1,275 @@
+#include "pmsystem.h"
+#include "../Utilities/otherwidgets.h"
+#include "../Utilities/functions.h"
+
+// TO-DO: Begin able to drag a tab outside of it's tab widget to separate the PM. I didn't do it because i
+// think it would break part of the new system.
+
+PMSystem::PMSystem(bool withTabs)
+{
+    QVBoxLayout *myLayout = new QVBoxLayout(this);
+    setWindowTitle(tr("Private Messages"));
+    setMinimumSize(300, 300);
+    myPMs = new QExposedTabWidget();
+    myPMs->setTabsClosable(true);
+    myPMs->setMovable(true);
+    myLayout->addWidget(myPMs);
+    setLayout(myLayout);
+
+    tabbedPMs = withTabs;
+
+    connect(myPMs, SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
+    connect(myPMs, SIGNAL(currentChanged(int)), this, SLOT(tabChanged(int)));
+}
+
+PMSystem::~PMSystem()
+{
+}
+
+void PMSystem::startPM(PMStruct *newPM)
+{
+    if(tabbedPMs) {
+        myPMs->addTab(newPM, newPM->name());
+        connect(newPM, SIGNAL(messageReceived(PMStruct*)), this, SLOT(messageReceived(PMStruct*)));
+        checkTabbing();
+    } else {
+        newPM->show();
+    }
+    myPMWindows.insert(newPM->id(), newPM);
+}
+
+void PMSystem::closeTab(int tabNum)
+{
+    PMStruct *widget = (PMStruct*) myPMs->widget(tabNum);
+    widget->deleteLater();
+    myPMs->removeTab(tabNum);
+    myPMWindows.remove(widget->id());
+    checkTabbing();
+}
+
+void PMSystem::checkTabbing()
+{
+    if(myPMs->count() <= 0) {
+        close();
+    } else {
+        if(myPMs->count() >= 1) {
+            show();
+        }
+    }
+}
+
+void PMSystem::tabChanged(int tabNum)
+{
+    // We're avoiding crash if we don't have any tab on the myPMs.
+    if(myPMs->count() <= 0) {
+        return;
+    }
+    PMStruct *widget = (PMStruct*) myPMs->widget(tabNum);
+    widget->state = PMStruct::NoMessages;
+    myPMs->tabBar()->setTabTextColor(tabNum, myPMs->tabBar()->palette().text().color());
+}
+
+void PMSystem::togglePMs(bool toggled)
+{
+    tabbedPMs = toggled;
+    if(tabbedPMs) {
+        foreach(PMStruct *pm, myPMWindows) {
+            // Otherwise it wont be added to the myPMs :(
+            pm->setWindowFlags(Qt::Widget);
+            myPMs->addTab(pm, pm->name());
+            show();
+        }
+    } else {
+        foreach(PMStruct *pm, myPMWindows) {
+            while(myPMs->count() > 0) {
+                myPMs->removeTab(0);
+            }
+            // Otherwise new window wouldn't show :(
+            pm->setWindowFlags(Qt::Window);
+            pm->show();
+            hide();
+        }
+    }
+}
+
+void PMSystem::messageReceived(PMStruct *pm) {
+    if(isVisible()) {
+        if(pm->state == PMStruct::NewMessage) {
+            return;
+        }
+        if(pm == myPMs->currentWidget()) {
+            return;
+        }
+        pm->state = PMStruct::NewMessage;
+        for(int i = 0; i < myPMs->count(); i++) {
+            if(myPMs->widget(i) == pm) {
+                myPMs->tabBar()->setTabTextColor(i, QColor(Qt::red));
+            }
+        }
+    } else {
+        show();
+        for(int i = 0; i < myPMs->count(); i++) {
+            if(myPMs->widget(i) == pm) {
+                myPMs->setCurrentIndex(i);
+            }
+        }
+    }
+
+}
+
+PMStruct::PMStruct(int id, const QString &ownName, const QString &name, const QString &content, bool html, bool pmDisabled, int starterAuth)
+    : m_ownName(ownName), escape_html(!html)
+{
+    setAttribute(Qt::WA_DeleteOnClose, true);
+
+    this->id() = id;
+    changeName(name);
+
+    QGridLayout *l = new QGridLayout(this);
+    this->setLayout(l);
+
+    m_mainwindow = new QScrollDownTextBrowser();
+    m_textToSend = new QIRCLineEdit();
+
+    l->addWidget(m_mainwindow, 0,0,1,2);
+    l->addWidget(m_textToSend, 1,0,1,2);
+
+    m_challenge = new QPushButton(tr("&Challenge"));
+    m_send = new QPushButton(tr("&Ignore"));
+    m_send->setCheckable(true);
+
+    if(pmDisabled) {
+        if(starterAuth <= 0) {
+            m_textToSend->setDisabled(true);
+            m_textToSend->setText(tr("You have private messages disabled."));
+            m_send->setDisabled(true);
+        }
+    } else {
+        m_textToSend->setDisabled(false);
+        m_textToSend->setText("");
+        m_send->setDisabled(false);
+    }
+
+    l->addWidget(m_challenge,2,0);
+    l->addWidget(m_send,2,1);
+
+    printLine(content, false);
+
+    connect(m_textToSend, SIGNAL(returnPressed()), this, SLOT(sendMessage()));
+    connect(m_send, SIGNAL(toggled(bool)), this, SLOT(ignore(bool)));
+    connect(m_challenge, SIGNAL(clicked()), this, SLOT(challenge()));
+}
+
+void PMStruct::changeName(const QString &newname)
+{
+    this->m_name = newname;
+    setWindowTitle(newname);
+}
+
+void PMStruct::changeSelf(const QString &newname)
+{
+    this->m_ownName = newname;
+}
+
+void PMStruct::printLine(QString line, bool self)
+{
+    if (line.trimmed().length() == 0)
+        return;
+
+    QSettings s;
+    bool tt = s.value("show_timestamps2").toBool();
+    QString timeStr = "";
+
+    if (tt)
+        timeStr += "(" + QTime::currentTime().toString("hh:mm") + ") ";
+
+    if (escape_html) {
+        line = escapeHtml(line);
+    }
+
+    if (self) {
+        printHtml(toColor(timeStr + "<b>" + escapeHtml(m_ownName) + ": </b>", Qt::darkBlue) + line, false);
+    } else {
+        printHtml(toColor(timeStr + "<b>" + escapeHtml(name()) + ": </b>", Qt::darkGray) + line, false);
+//        if (!QApplication::activeWindow()) {
+//            QApplication::alert(this, 10000);
+//            //raise();
+//        }
+    }
+}
+
+void PMStruct::printHtml(const QString &htmlCode, bool timestamps)
+{
+    QSettings s;
+    bool tt = s.value("show_timestamps2").toBool();
+    QString timeStr = "";
+
+    if (tt && timestamps)
+        timeStr += "(" + QTime::currentTime().toString("hh:mm") + ") ";
+
+    m_mainwindow->insertHtml(timeStr + removeTrollCharacters(htmlCode) + "<br />");
+    emit messageReceived(this);
+}
+
+void PMStruct::sendMessage()
+{
+    QString str = m_textToSend->text().trimmed();
+    m_textToSend->clear();
+
+    if (str.length() == 0) {
+        return;
+    }
+
+    if (str.length() > 0) {
+        QStringList s = str.split('\n');
+        foreach(QString s1, s) {
+            if (s1.length() > 0) {
+                emit messageEntered(id(), s1);
+                printLine(s1, true);
+            }
+        }
+    }
+}
+
+void PMStruct::ignore(bool yes)
+{
+    emit ignore(id(), yes);
+}
+
+void PMStruct::challenge()
+{
+    emit challengeSent(id());
+}
+
+void PMStruct::disablePM(bool b, int starterAuth) {
+    if(b == 1) {
+        if(starterAuth <= 0) {
+            m_textToSend->setDisabled(true);
+            m_textToSend->setText(tr("You have private messages disabled."));
+            m_send->setDisabled(true);
+        }
+    } else {
+        m_textToSend->setDisabled(false);
+        m_textToSend->setText("");
+        m_send->setDisabled(false);
+    }
+}
+
+void PMStruct::disable()
+{
+    printHtml("<i>" + tr("The other party left the server, so the window was disabled.") + "</i>");
+    m_challenge->setDisabled(true);
+    m_send->setDisabled(true);
+    m_textToSend->setDisabled(true);
+}
+void PMStruct::reuse(int id)
+{
+    if (this->id() == id) return;
+
+    this->id() = id;
+    printHtml("<i>" + tr("The player has logged on again") + "</i>");
+    m_challenge->setEnabled(true);
+    m_send->setEnabled(true);
+    m_textToSend->setEnabled(true);
+}
+
---
+++ src/Teambuilder/pmsystem.h
@@ -0,0 +1,87 @@
+#ifndef PMSYSTEM_H
+#define PMSYSTEM_H
+
+#include <QtGui>
+
+#include "../Utilities/functions.h"
+#include "../Utilities/otherwidgets.h"
+
+class QScrollDownTextBrowser;
+class QIRCLineEdit;
+class PMStruct;
+
+class PMSystem : public QWidget {
+    Q_OBJECT
+
+public:
+    PMSystem(bool withTabs);
+    ~PMSystem();
+
+    void startPM(PMStruct *newPM);
+    void checkTabbing();
+
+private slots:
+    void closeTab(int tabNum);
+    void tabChanged(int tabNum);
+    void togglePMs(bool toggled);
+    void messageReceived(PMStruct *pm);
+
+private:
+    QExposedTabWidget *myPMs;
+    bool tabbedPMs;
+    QHash<int, PMStruct*> myPMWindows;
+};
+
+struct PMStruct : public QWidget
+{
+    Q_OBJECT
+    PROPERTY(int, id);
+public:
+    PMStruct(int id, const QString &ownName, const QString &name, const QString &content = "", bool html = false, bool pmDisabled = false, int starterAuth = 0);
+    ~PMStruct() {
+        emit destroyed(id(), m_name);
+    }
+
+    void changeName(const QString &newname);
+    void changeSelf(const QString &newname);
+    void printLine(QString line, bool self = false);
+    void disable();
+    void reuse(int id);
+
+    QString name() const {
+        return m_name;
+    }
+
+    enum State {
+        NoMessages,
+        NewMessage
+    };
+
+    int state;
+
+signals:
+    void messageReceived(PMStruct *pm);
+    void messageEntered(int id, const QString &mess);
+    void challengeSent(int id);
+    void destroyed(int id, QString name);
+    void ignore(int id, bool);
+
+public slots:
+    void sendMessage();
+    void ignore(bool);
+    void challenge();
+    void disablePM(bool, int starterAuth);
+
+private:
+    QString m_name;
+    QString m_ownName;
+    bool escape_html;
+
+    void printHtml(const QString &htmlCode, bool timestamps = true);
+
+    QScrollDownTextBrowser *m_mainwindow;
+    QIRCLineEdit *m_textToSend;
+    QPushButton *m_challenge, *m_send;
+};
+
+#endif // PMSYSTEM_H
