bf1b5f75f1529dbef6f68cb10319702df7ffa886
coyotte508 <coyotte508@hotmail.com>
2012-01-20 03:12:50 +0100

    fixed server's zip packets

--- src/Server/networkutilities.h
+++ src/Server/networkutilities.h
@@ -5,17 +5,24 @@
 
 template <typename ...Params>
 QByteArray makeZipPacket(int command, Params&&... params) {
-    QByteArray tosend(6, Qt::Uninitialized);
+    QByteArray tosend;
     DataStream out(&tosend, QIODevice::WriteOnly);
 
     out.pack(uchar(command), std::forward<Params>(params)...);
 
-    QByteArray ret;
-    ret.push_back('\0'); /* ZipCommand == 0 */
-    ret.push_back('\0'); /* 0 = Single command, 1 would be multiple packets */
     QByteArray cp = qCompress(tosend);
 
-    ret.push_back(cp);
+    QByteArray ret(6+cp.length(), Qt::Uninitialized);
+
+    const int l = ret.length()-4;
+    ret[0] = l >> (3*8);
+    ret[1] = l >> (2*8);
+    ret[2] = l >> 8;
+    ret[3] = l;
+    ret[4] = '\0'; /* ZipCommand == 0 */
+    ret[5] = '\0'; /* 0 = Single command, 1 would be multiple packets */
+
+    ret.replace(6, cp.length(), cp);
     return ret;
 }
 
