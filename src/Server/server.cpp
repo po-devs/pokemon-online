42de3b47677adbd233459d2ec9b8cc2e66acc855
coyotte508 <coyotte508@hotmail.com>
2012-02-26 16:04:17 +0100

    made separate DNS for test registry

--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -506,7 +506,7 @@ void Server::connectToRegistry()
     printLine("Connecting to registry...");
 
     QTcpSocket * s = new QTcpSocket(NULL);
-    s->connectToHost("pokemon-online.dynalias.net", 8081);
+    s->connectToHost("pokemon-online-registry.dynalias.net", 8081);
 
     connect(s, SIGNAL(connected()), this, SLOT(regConnected()));
     connect(s, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(regConnectionError()));
