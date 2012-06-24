88a69221aabd7d9b9fb2b4b0898ac328a16439b7
Toni Fadjukoff <lamperi@lamperi.name>
2012-06-25 00:55:22 +0300

    Implemented http server for registry.
    Now, it is missing a lot of important stuff, most important being ability to
    protect it with a password.

--- src/Registry/webinterface.h
+++ src/Registry/webinterface.h
@@ -1,15 +1,40 @@
 #ifndef WEBINTERFACE_H
 #define WEBINTERFACE_H
 
+#include "HttpServer.h"
+#include "HttpHandler.h"
+#include "HttpHandlerSimpleRouter.h"
+#include "HttpConnection.h"
+#include <QObject>
 
+class Registry;
 
-class RegistryWebInterface
+class RegistryWebInterface : QObject
 {
+Q_OBJECT
+
+public:
+
     RegistryWebInterface(Registry *reg);
+    virtual ~RegistryWebInterface();
 
 private slots:
 
-    void handleRequest();
+    void showServers(Pillow::HttpConnection*);
+    void showBans(Pillow::HttpConnection*);
+    void updateBans(Pillow::HttpConnection*);
+    void showAnnouncement(Pillow::HttpConnection*);
+    void updateAnnouncement(Pillow::HttpConnection*);
+
+private:
+
+    QString escapeHtml(QString const&);
+    QMap<QString,QString> parsePost(QByteArray const&);
+
+    Registry *regptr;
+
+    Pillow::HttpServer server;
+    Pillow::HttpHandlerSimpleRouter router;
 
 };
 
