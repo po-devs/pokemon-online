4ae78b17485707dfdb96142f62293afce7bfa488
coyotte508 <coyotte508@hotmail.com>
2012-06-25 15:48:16 +0200

    Added base for new client scripting plugin

---
+++ src/ClientScripting/clientscripting.h
@@ -0,0 +1,29 @@
+#ifndef CLIENTSCRIPTING_H
+#define CLIENTSCRIPTING_H
+
+#include "ClientScripting_global.h"
+#include "../Teambuilder/plugininterface.h"
+
+extern "C" {
+CLIENTSCRIPTINGSHARED_EXPORT ClientPlugin *createPluginClass(MainEngineInterface*);
+}
+
+class CLIENTSCRIPTINGSHARED_EXPORT ClientScripting : public ClientPlugin {
+public:
+    ClientScripting();
+
+    /* The name of the option the plugin would take in the menu bar.
+       Also appears as the name of the plugin */
+    QString pluginName() const;
+
+    /* A widget that would be used to configure the plugin in the menu bar.
+       Return NULL if you don't want one (default behavior) */
+    QWidget * getConfigurationWidget();
+
+    bool hasConfigurationWidget() const;
+private:
+
+};
+
+
+#endif // CLIENTSCRIPTING_H
