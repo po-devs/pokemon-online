87c391383d84cc32e0874c3dfeb9716c4a3a5e28
coyotte508 <coyotte508@hotmail.com>
2012-07-02 21:40:05 +0200

    Now server can change mods

---
+++ src/Server/modswindow.cpp
@@ -0,0 +1,47 @@
+#include "modswindow.h"
+#include "ui_modswindow.h"
+#include "../PokemonInfo/pokemoninfo.h"
+
+ModsWindow::ModsWindow(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ModsWindow)
+{
+    ui->setupUi(this);
+    setAttribute(Qt::WA_DeleteOnClose, true);
+
+    connect(ui->buttonBox, SIGNAL(accepted()), SLOT(accepted()));
+    connect(ui->buttonBox, SIGNAL(accepted()), SLOT(close()));
+    connect(ui->buttonBox, SIGNAL(rejected()), SLOT(close()));
+
+    ui->comboBox->addItem("No Mods");
+
+    QSettings s;
+    QString currentMod = s.value("Mods/CurrentMod").toString();
+
+    foreach(QString mod, PokemonInfoConfig::availableMods(FillMode::Server)) {
+        ui->comboBox->addItem(mod);
+
+        if (mod == currentMod) {
+            ui->comboBox->setCurrentIndex(ui->comboBox->count()-1);
+        }
+    }
+
+    show();
+}
+
+void ModsWindow::accepted()
+{
+    QSettings s;
+
+    QString mod = ui->comboBox->currentIndex() == 0 ? QString() : ui->comboBox->currentText();
+
+    if (mod != s.value("Mods/CurrentMod")) {
+        s.setValue("Mods/CurrentMod", mod);
+        emit modChanged(mod);
+    }
+}
+
+ModsWindow::~ModsWindow()
+{
+    delete ui;
+}
