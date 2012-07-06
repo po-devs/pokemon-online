d50f1f026bb0d4b225a62cb7652683ba7880973e
coyotte508 <coyotte508@hotmail.com>
2012-07-06 05:02:09 +0200

    v2.0.01

--- src/Shared/config.h
+++ src/Shared/config.h
@@ -3,10 +3,10 @@
 
 #include <qglobal.h>
 
-#define VERSION QString("2.0.00")
+#define VERSION QString("2.0.01")
 
 static const quint16 PROTOCOL_VERSION = 0;
 static const quint16 PROTOCOL_SUBVERSION = 0;
-static const quint16 CLIENT_VERSION_NUMBER = 0;
+static const quint16 CLIENT_VERSION_NUMBER = 2001;
 
 #endif // CONFIG_H
