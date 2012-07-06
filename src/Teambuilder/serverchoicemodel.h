e75017a773e6f0ce80703f159a04fb161ab98cc1
coyotte508 <coyotte508@hotmail.com>
2012-07-06 08:38:48 +0200

    compile error in serverchoicemodel

--- src/Teambuilder/serverchoicemodel.h
+++ src/Teambuilder/serverchoicemodel.h
@@ -9,7 +9,7 @@ class ServerChoiceModel : public QAbstractTableModel
 {
     Q_OBJECT
 public:
-    int rowCount(const QModelIndex &parent) const;
+    int rowCount(const QModelIndex &parent=QModelIndex()) const;
     int columnCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
