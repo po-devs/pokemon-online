070fdb7dc101e8ce0e8297aee3cd1cd3483998ab
coyotte508 <coyotte508@hotmail.com>
2012-07-09 23:40:49 +0200

    Added BW2 dw moves

--- src/ChainBreeding/main.cpp
+++ src/ChainBreeding/main.cpp
@@ -65,7 +65,7 @@ int main(int, char**)
     MoveSetChecker::init("db/pokes/");
     MoveInfo::init("db/moves/");
 
-    Pokemon::gen gen(5, 1);
+    Pokemon::gen gen(5, -1);
 
     qDebug() << "Gen " << GenInfo::Version(gen);
     qDebug() << "";
