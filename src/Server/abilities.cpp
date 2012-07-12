f5cffdfbf2ab5f56feb6de04633bbcebe3169456
coyotte508 <coyotte508@hotmail.com>
2012-07-12 06:23:46 +0200

    Fixed magic mirror to allow attacking after bouncing

--- src/Server/abilities.cpp
+++ src/Server/abilities.cpp
@@ -1578,26 +1578,21 @@ struct AMMagicMirror : public AM
         /* Now Bouncing back ... */
         BS::context ctx = turn(b,target);
         BS::BasicMoveInfo info = tmove(b,target);
+        BS::TurnMemory turnMem = fturn(b, target);
 
         turn(b,target).clear();
         MoveEffect::setup(move,target,s,b);
-
-        bool hadMoved = fturn(b,s).contains(TM::HasMoved);
 
         turn(b,target)["Target"] = s;
         b.battleMemory()["CoatingAttackNow"] = true;
         b.useAttack(target,move,true,false);
         b.battleMemory().remove("CoatingAttackNow");
 
-        /* Magic mirror shouldn't cancel the attack */
-        if (!hadMoved) {
-            fturn(b,s).remove(TM::HasMoved);
-        }
-
         /* Restoring previous state. Only works because moves reflected don't store useful data in the turn memory,
             and don't cause any such data to be stored in that memory */
         turn(b,target) = ctx;
         tmove(b,target) = info;
+        fturn(b,target) = turnMem;
     }
 };
 
