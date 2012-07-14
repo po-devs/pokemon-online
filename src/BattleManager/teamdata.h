c8d7739a8d0834a440bb841efb07c723792dc1f9
coyotte508 <coyotte508@hotmail.com>
2012-07-14 20:34:33 +0200

    Added more accessibility to scripts/battle window

--- src/BattleManager/teamdata.h
+++ src/BattleManager/teamdata.h
@@ -14,14 +14,17 @@ public:
     ShallowBattlePoke* poke(int slot);
     QString& name();
     QString name() const;
+    Pokemon::gen gen() const {return mGen;}
 
     void setPoke(int slot, const ShallowBattlePoke* poke);
     void setPoke(int slot, const PokeBattle* poke);
     void switchPokemons(int slot1, int slot2);
     void setTeam(const TeamBattle *team);
+    void setGen(Pokemon::gen gen);
 protected:
     std::vector< ShallowBattlePoke* > pokemons;
     QString mName;
+    Pokemon::gen mGen;
 };
 
 #endif // TEAMDATA_H
