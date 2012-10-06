39a8c4b76badd392e0b0e299974560cdb1ed1db4
Dev Gurjar <d25inferno@gmail.com>
2012-10-06 19:26:23 -0400

    Added skeleton code for the plugin

--- src/SmogonPlugin/smogonplugin.cpp
+++ src/SmogonPlugin/smogonplugin.cpp
@@ -1,11 +1,42 @@
 #include "smogonplugin.h"
+#include "../Teambuilder/engineinterface.h"
+#include "../Teambuilder/Teambuilder/teamholderinterface.h"
+#include "../PokemonInfo/pokemonstructs.h"
+#include "../Utilities/otherwidgets.h"
+#ifdef _WIN32
+#include "../../SpecialIncludes/zlib.h"
+#include "../../SpecialIncludes/qrencode.h"
+#else
+#include <zlib.h>
+#include <qrencode.h>
+#endif
+#include <QLabel>
+#include <QBitmap>
+#include <QPalette>
 
-SmogonPlugin::SmogonPlugin(QWidget *parent)
-    : QWidget(parent)
+
+ClientPlugin* createPluginClass(MainEngineInterface *interface)
+{
+    return new SmogonPlugin(interface);
+}
+
+
+SmogonPlugin::SmogonPlugin(MainEngineInterface *interface) : interface(interface)
 {
 }
 
-SmogonPlugin::~SmogonPlugin()
+bool SmogonPlugin::hasConfigurationWidget() const
 {
-    
+    return true;
 }
+
+QString SmogonPlugin::pluginName() const
+{
+    return "Get builds from Smogon";
+}
+
+QWidget *SmogonPlugin:getConfigurationWidget()
+{
+
+}
+
