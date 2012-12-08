c2bd7fae7a4d7ea81595cd56365d1bb735352d7b
coyotte508 <coyotte508@hotmail.com>
2012-12-08 02:31:19 +0100

    Fix protocol for relay station

--- src/RelayStation/dualwielder.cpp
+++ src/RelayStation/dualwielder.cpp
@@ -381,6 +381,7 @@ void DualWielder::readSocket(const QByteArray &commandline)
         if (f[0]) {
             BattleConfiguration conf;
             if (version < ProtocolVersion(1,0)) {
+                qDebug() << "old deserializing";
                 conf.oldDeserialize(in);
             } else {
                 in >> conf;
