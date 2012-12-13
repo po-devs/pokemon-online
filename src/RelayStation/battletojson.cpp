b3fca28f5287dc8ad5c221a00a1946f6f6d81327
coyotte508 <coyotte508@hotmail.com>
2012-12-13 14:44:41 +0100

    added some more battle comands to relay to webclient

--- src/RelayStation/battletojson.cpp
+++ src/RelayStation/battletojson.cpp
@@ -84,10 +84,24 @@ void BattleToJson::onMajorStatusChange(int spot, int status, bool multipleTurns,
     map.insert("silent", silent);
 }
 
-void BattleToJson::onDamageDone(int spot, int damage)
+//    void onPokeballStatusChanged(int player, int poke, int status);
+
+void BattleToJson::onStatusAlreadyThere(int spot, int status)
 {
-    makeCommand("damage");
-    map.insert("damage", damage);
+    makeCommand("alreadystatus");
+    map.insert("status", status);
+}
+
+void BattleToJson::onStatusNotification(int spot, int status)
+{
+    makeCommand("feelstatus");
+    map.insert("status", status);
+}
+
+void BattleToJson::onStatusOver(int spot, int status)
+{
+    makeCommand("freestatus");
+    map.insert("status", status);
 }
 
 void BattleToJson::onStatusDamage(int spot, int status)
@@ -95,8 +109,6 @@ void BattleToJson::onStatusDamage(int spot, int status)
     makeCommand("statusdamage");
     map.insert("status", status);
 }
-
-//    void onStatusOver(int spot, int status);
 
 void BattleToJson::onAttackFailing(int spot, bool silent)
 {
@@ -153,3 +165,88 @@ void BattleToJson::onDrained(int spot)
 {
     makeCommand("drain");
 }
+
+void BattleToJson::onStartWeather(int spot, int weather, bool ability)
+{
+    makeCommand("weatherstart");
+    map.insert("weather", weather);
+    map.insert("permanent", ability);
+}
+
+void BattleToJson::onContinueWeather(int weather)
+{
+    map.insert("command", "feelweather");
+    map.insert("weather", weather);
+}
+
+void BattleToJson::onEndWeather(int weather)
+{
+    map.insert("command", "weatherend");
+    map.insert("weather", weather);
+}
+
+void BattleToJson::onHurtWeather(int spot, int weather)
+{
+    makeCommand("weatherhurt");
+    map.insert("weather", weather);
+}
+
+void BattleToJson::onDamageDone(int spot, int damage)
+{
+    makeCommand("damage");
+    map.insert("damage", damage);
+}
+
+//    void onAbilityMessage(int spot, int ab, int part, int type, int foe, int other);
+
+void BattleToJson::onSubstituteStatus(int spot, bool substitute)
+{
+    makeCommand("substitute");
+    map.insert("substitute", substitute);
+}
+
+//    void onBlankMessage();
+//    void onClauseActivated(int clause);
+
+void BattleToJson::onRatedNotification(bool rated)
+{
+    map.insert("command", "rated");
+    map.insert("rated", rated);
+}
+
+void BattleToJson::onTierNotification(const QString &tier)
+{
+    map.insert("command", "tier");
+    map.insert("tier", tier);
+}
+
+//    void onDynamicInfo(int spot, const BattleDynamicInfo &info);
+//    void onPokemonVanish(int spot);
+//    void onPokemonReappear(int spot);
+//    void onSpriteChange(int spot, int newSprite);
+//    void onDefiniteFormeChange(int spot, int poke, int newPoke);
+//    void onCosmeticFormeChange(int spot, int subforme);
+//    void onClockStart(int player, int time);
+//    void onClockStop(int player, int time);
+//    void onShiftSpots(int player, int spot1, int spot2, bool silent);
+//    void onBattleEnd(int res, int winner);
+//    void onPPChange(int spot, int move, int PP);
+//    void onOfferChoice(int player, const BattleChoices &choice);
+//    void onTempPPChange(int spot, int move, int PP);
+//    void onMoveChange(int spot, int slot, int move, bool definite);
+//    void onRearrangeTeam(int player, const ShallowShownTeam& team);
+//    void onChoiceSelection(int player);
+//    void onChoiceCancellation(int player);
+//    void onVariation(int player, int bonus, int malus);
+//    void onDynamicStats(int spot, const BattleStats& stats);
+//    void onPrintHtml(const QString &html);
+//    void onReconnect(int player);
+//    void onDisconnect(int player);
+//    void onAttackChosen(int spot, int attackSlot, int target);
+//    void onSwitchChosen(int spot, int pokeSlot);
+//    void onTeamOrderChosen(int player, const RearrangeChoice &rearrange);
+//    void onChoiceCancelled(int player);
+//    void onShiftToCenterChosen(int player);
+//    void onDrawRequest(int player);
+//    void onUseItem(int player, int item);
+//    void onItemChangeCount(int player, int item, int count);
