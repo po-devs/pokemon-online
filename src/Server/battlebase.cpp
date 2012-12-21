f87b270542ba93a7f8238ec610aa191aed9620f7
coyotte508 <coyotte508@hotmail.com>
2012-12-21 23:24:33 +0100

    Fix problem of battlers doing twice the same move
    Dirty fix, but w/e. When enough people have updated to next release, that
    commit will be reverted.

--- src/Server/battlebase.cpp
+++ src/Server/battlebase.cpp
@@ -16,6 +16,8 @@ BattleBase::BattleBase()
     timer = NULL;
 }
 
+bool oldBattleNotifications = true;
+
 void BattleBase::init(Player &p1, Player &p2, const ChallengeInfo &c, int id, int nteam1, int nteam2, PluginManager *pluginManager)
 {
     publicId() = id;
@@ -736,7 +738,7 @@ void BattleBase::notifyChoices(int p)
     }
 
     if (canMove) {
-        notify(All, StartChoices, p);
+        notify(oldBattleNotifications ? p : All, StartChoices, p);
     }
 }
 
@@ -1419,11 +1421,11 @@ void BattleBase::requestChoices()
     }
 
     if (!allChoicesOkForPlayer(Player1)) {
-        notify(All, StartChoices, Player1);
+        notify(oldBattleNotifications ? Player1 : All, StartChoices, Player1);
     }
 
     if (!allChoicesOkForPlayer(Player2)) {
-        notify(All, StartChoices, Player2);
+        notify(oldBattleNotifications ? Player2: All, StartChoices, Player2);
     }
 
     if (count > 0) {
@@ -1465,7 +1467,7 @@ bool BattleBase::requestChoice(int slot, bool acquire, bool custom)
     startClock(player);
 
     if (acquire) {
-        notify(All, StartChoices, player);
+        notify(oldBattleNotifications ? player : All, StartChoices, player);
         yield();
     }
 
@@ -1633,11 +1635,11 @@ void BattleBase::requestSwitchIns()
         }
 
         if (!allChoicesOkForPlayer(Player1)) {
-            notify(All, StartChoices, Player1);
+            notify(oldBattleNotifications ? Player1: All, StartChoices, Player1);
         }
 
         if (!allChoicesOkForPlayer(Player2)) {
-            notify(All, StartChoices, Player2);
+            notify(oldBattleNotifications ? Player2 :All, StartChoices, Player2);
         }
 
         yield();
