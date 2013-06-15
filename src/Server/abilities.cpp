07c2b2ddebb5375c2ffbf3f91057c048de0ccdfb
Fuzzysqurl <risuke@comcast.net>
2013-06-15 14:00:30 -0500

    Volt/Water Absorb fix with Heal Block
    Still working on the leech seed fix, which might take a bit longer

--- src/Server/abilities.cpp
+++ src/Server/abilities.cpp
@@ -1072,8 +1072,7 @@ struct AMVoltAbsorb : public AM {
     static void op(int s, int t, BS &b) {
         if (type(b,t) == poke(b,s)["AbilityArg"].toInt() && (b.gen() >= 4 || tmove(b,t).power > 0) ) {
             turn(b,s)[QString("Block%1").arg(b.attackCount())] = true;
-
-            if (b.poke(s).lifePoints() == b.poke(s).totalLifePoints()) {
+            if (b.poke(s).lifePoints() == b.poke(s).totalLifePoints() || poke(b, s).value("HealBlockCount").toInt() > 0) {
                 b.sendAbMessage(70,1,s,s,type(b,t), b.ability(s));
             } else {
                 b.sendAbMessage(70,0,s,s,type(b,t), b.ability(s));
