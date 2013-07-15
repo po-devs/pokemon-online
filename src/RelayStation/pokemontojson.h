39f5fa970419d1a9544dd1cf02a21dc645ed48e0
coyotte508 <coyotte508@hotmail.com>
2013-07-15 18:30:07 +0200

    relay station transmits PP change & team preview

--- src/RelayStation/pokemontojson.h
+++ src/RelayStation/pokemontojson.h
@@ -11,11 +11,15 @@ class TeamBattle;
 class BattleMove;
 class BattleChoice;
 class BattleChoices;
+class ShallowShownTeam;
+class ShallowShownPoke;
 
 QVariantMap toJson(const Pokemon::gen &gen);
 QVariantMap toJson(const BattleConfiguration & conf);
 QVariantMap toJson(const BattleChoices &choices);
 QVariantMap toJson(const ShallowBattlePoke &poke);
+QVariant toJson(const ShallowShownTeam &team);
+QVariantMap toJson(const ShallowShownPoke &poke);
 QVariant toJson(const TeamBattle &team);
 QVariantMap toJson(const PokeBattle &poke);
 QVariantMap toJson(const BattleMove &move);
