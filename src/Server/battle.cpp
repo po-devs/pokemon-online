0288605f884aa8b20d3c321edbdb5bb2fc1f6e03
Fuzzysqurl <risuke@comcast.net>
2013-09-05 00:33:29 -0500

    Reverting 0 damage fix

--- src/Server/battle.cpp
+++ src/Server/battle.cpp
@@ -2898,7 +2898,7 @@ void BattleSituation::inflictDamage(int player, int damage, int source, bool str
         callieffects(player, source, "BeforeTakingDamage");
     }
 
-    if (damage == 0 && (gen() <= 4 || !straightattack)) {
+    if (damage == 0 /*&& (gen() <= 4 || !straightattack)*/) {
         damage = 1;
     }
 
