0c3a9a570cbc104010a2d72fa3c8d9b4d47c3f52
coyotte508 <coyotte508@gmail.com>
2013-10-06 14:30:42 +0200

    fixes

--- src/Shared/config.h
+++ src/Shared/config.h
@@ -3,11 +3,11 @@
 
 #include <qglobal.h>
 
-#define VERSION QString("2.1.0")
+#define VERSION QString("2.2.0")
 
 static const quint16 PROTOCOL_VERSION = 2;
 static const quint16 PROTOCOL_SUBVERSION = 1;
-static const quint16 CLIENT_VERSION_NUMBER = 2100;
+static const quint16 CLIENT_VERSION_NUMBER = 2200;
 static const int UPDATE_ID = 4;
 
 #ifdef Q_OS_LINUX
