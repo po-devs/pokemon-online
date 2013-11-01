6adcf9a8e14a462ad5f52ee14c9cf53a7235ce74
coyotte508 <coyotte508@gmail.com>
2013-11-01 22:22:37 +0100

    Megavos can't be launched directly into battle

--- src/PokemonInfo/pokemonstructs.cpp
+++ src/PokemonInfo/pokemonstructs.cpp
@@ -193,6 +193,10 @@ void PokePersonal::runCheck()
         return;
     }
 
+    if (PokemonInfo::IsMegaEvo(num())) {
+        num() = num().original();
+    }
+
     if (gen() <= 2) {
         ability() = 0;
         nature() = 0;
