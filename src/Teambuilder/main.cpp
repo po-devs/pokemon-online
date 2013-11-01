591a5384f653afdcbba1cc5be33744091769c54d
coyotte508 <coyotte508@gmail.com>
2013-11-01 17:25:18 +0100

    Moved translation files to easier path

--- src/Teambuilder/main.cpp
+++ src/Teambuilder/main.cpp
@@ -237,7 +237,7 @@ int main(int argc, char *argv[])
                 QLibraryInfo::location(QLibraryInfo::TranslationsPath));
         a.installTranslator(&qtTranslator);
 
-        translator.load(QString("trans/%1/translation_%1").arg(locale));
+        translator.load(QString("trans/translation_%1").arg(locale));
         a.installTranslator(&translator);
 
         MainEngine w(updated);
