5db3a18a1d7334cdf9490121f0a62c366fe68d11
Fuzzysqurl <risuke@comcast.net>
2015-05-12 01:21:08 -0500

    Use the right brackets...

--- bin/qml/BattleDataQML/status.js
+++ bin/qml/BattleDataQML/status.js
@@ -1,7 +1,7 @@
 var statusArray = {};
 
-statusArray[PokeData.Poisoned] = " {PSN}";
-statusArray[PokeData.Koed] = " {KO]";
+statusArray[PokeData.Poisoned] = " [PSN]";
+statusArray[PokeData.Koed] = " [KO]";
 statusArray[PokeData.Frozen] = " [FRZ]";
 statusArray[PokeData.Asleep] = " [SLP]";
 statusArray[PokeData.Burnt] = " [BRN]";
