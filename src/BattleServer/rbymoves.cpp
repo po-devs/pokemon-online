9488ef0a87afb8f6c0676b895037fcff9951a97c
Berkan Diler <thetoerk@gmail.com>
2016-06-04 13:11:20 +0200

    Fix Stadium Mimic PP (#1475)
    * Fix Stadium Mimic PP
    
    Make sure that the PP of Mimic doesn't raise when you use it, switch to
    another poke, switch back and use it again.
    
    * Fix

--- src/BattleServer/rbymoves.cpp
+++ src/BattleServer/rbymoves.cpp
@@ -729,9 +729,10 @@ struct RBYMimic : public MM
             move = b.move(t, b.randint(4));
         }
         int slot = fpoke(b,s).lastMoveSlot;
+        int oldpp = b.PP(s, slot);
         b.changeTempMove(s, slot, move);
         if (b.isStadium()) {
-            b.changePP(s, slot, 15);
+            b.changePP(s, slot, oldpp);
         }
         b.sendMoveMessage(81,0,s,type(b,s),t,move);
     }
