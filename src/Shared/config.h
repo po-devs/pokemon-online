3b31616fb854b6826a3fceb6fc307da0d601571c
coyotte508 <coyotte508@gmail.com>
2016-06-26 03:33:39 +0200

    Add version diff notification

--- src/Shared/config.h
+++ src/Shared/config.h
@@ -6,7 +6,7 @@
 #define VERSION QString("2.6.3")
 
 static const quint16 PROTOCOL_VERSION = 3;
-static const quint16 PROTOCOL_SUBVERSION = 2;
+static const quint16 PROTOCOL_SUBVERSION = 3;
 static const quint16 CLIENT_VERSION_NUMBER = 2630;
 static const int UPDATE_ID = 4;
 
