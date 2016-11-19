6594fb324cbe4f92f2d2bb7533289e5f63a42258
Fuzzysqurl <risuke@comcast.net>
2016-11-19 00:27:24 -0600

    oops wrong order

--- src/BattleServer/abilities.cpp
+++ src/BattleServer/abilities.cpp
@@ -566,11 +566,11 @@ struct AMForeCast : public AM {
         }
 
         //To allow the type reset every turn
+        if (poke(b,s).contains("ForestTrick") || poke(b,s).contains("Soaked") || poke(b,s).contains("BurnedUp")) {
+            fpoke(b,s).types = QVector<int> () << PokemonInfo::Type1(b.poke(s).num(), b.gen()) << PokemonInfo::Type2(b.poke(s).num(), b.gen());
+        }
         if (poke(b,s).contains("BurnedUp")) {
             poke(b,s).remove("BurnedUp");
-        }
-        if (poke(b,s).contains("ForestTrick") || poke(b,s).contains("Soaked")) {
-            fpoke(b,s).types = QVector<int> () << PokemonInfo::Type1(b.poke(s).num(), b.gen()) << PokemonInfo::Type2(b.poke(s).num(), b.gen());
         }
 
         //Check required to prevent crash cause
